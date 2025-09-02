c22ac29dd542f38b8e08569e99dc4bdacb9e26f8
Rodolfo <rodolfo9254@gmail.com>
2025-09-02 20:33:49 -0300

    adc na main click listener

--- flagApp/app/src/main/java/br/ufpr/flagapp/MainActivity.kt
+++ flagApp/app/src/main/java/br/ufpr/flagapp/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnStart: Button = findViewById(R.id.btnStart)
+        val etName: EditText = findViewById(R.id.etName)
+
+        btnStart.setOnClickListener {
+            if (etName.text.isNotEmpty()) {
+                val intent = Intent(this, QuizActivity::class.java)
+                intent.putExtra("PLAYER_NAME", etName.text.toString())
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Por favor, digite seu nome", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
